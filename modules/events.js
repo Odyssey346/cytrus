cd5d452215062815c00ed0e53c2fcfeac0a1e9fe
DestroyedEDGE <44955931+DestroyedEDGE@users.noreply.github.com>
2019-07-04 07:29:09 -0600

    fixed some grammar mistakes and yeah thats it

--- modules/events.js
+++ modules/events.js
@@ -2,34 +2,34 @@
 const fs = require('fs');
 const util = require('util');
 
-//Define variubles
+//Define variables
 const promisify = util.promisify;
 const readdir = promisify(fs.readdir);
 
 module.exports = (client) => {
-  //Get event files
+//Get event files
   readdir(__dirname + '/../events/', (err, files) => {
-    //If their is an error, Return the error
+//If there is an error, return the error
     if (err) return client.logger.error(err);
 
-    //For each file in the file array run this function
+//For each file in the file array run this function
     files.forEach(file => {
-      //If the file extention (.py, .js, .md) is not js, Ignore it
+//If the file extention (.py, .js, .md) is not js, Ignore it
       if (!file.endsWith('.js')) return;
 
-      //Make the 'event' variuble the file object
+//Make the 'event' variuble the file object
       const event = require(`../events/${file}`);
 
-      //Split the file name from the file extention
+//Split the file name from the file extention
       let eventName = file.split('.')[0];
 
-      //When the event name is ran, Run the event files objects
+//When the event name is ran, Run the event files objects
       client.on(eventName, event.bind(null, client));
 
-      //Delete the event cache
+//Delete the event cache
       delete require.cache[require.resolve(`../events/${file}`)];
 
-      //Log that the event is loading
+//Log that the event is loading
       client.logger.log(`Loading event: ${eventName}`)
     });
     console.log();
