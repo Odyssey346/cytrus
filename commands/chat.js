2eaddf4f5ec0d936e072eb6ce9375c1d6a835523
Rexogamer <beartechtalks@gmail.com>
2019-07-04 13:23:42 +0100

    Update chat.js

--- commands/chat.js
+++ commands/chat.js
@@ -1,6 +1,6 @@
 exports.run = async (client, message, args, level) => {
   try {
-    if (!args[0]) return message.reply('You need to ask the Bot somthing!');
+    if (!args[0]) return message.reply('You need to say something to Cytrus!');
     
     let msg = await message.reply('Processing message... (This may take a while!)');
     client.cleverbot.create(async (err, session) => {
@@ -19,7 +19,7 @@ exports.run = async (client, message, args, level) => {
 
 exports.conf = {
   enabled: true,
-  aliases: ['ai'],
+  aliases: ['ai', 'aichat'],
   guildOnly: false,
   permLevel: 'User'
 };
@@ -27,6 +27,6 @@ exports.conf = {
 exports.help = {
   name: 'chat',
   category: 'Fun',
-  description: 'Uses AI to chat with you',
+  description: 'Uses AI to chat with you.',
   usage: 'chat <message>'
 };
